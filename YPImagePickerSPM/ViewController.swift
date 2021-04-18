399596706fec448a917d28530732eedc6c642d56
Fernando Valverde <fernando@visualcosita.com>
2021-04-18 15:07:57 -0600

    Reproducible error with v4.6.0

--- YPImagePickerSPM/ViewController.swift
+++ YPImagePickerSPM/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import YPImagePicker
 
 class ViewController: UIViewController {
 
@@ -14,6 +15,27 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func presentImagePicker(_ sender: Any) {
+        var config = YPImagePickerConfiguration()
+        config.shouldSaveNewPicturesToAlbum = false
+        config.startOnScreen = YPPickerScreen.library
+        config.library.onlySquare = false
+        config.library.isSquareByDefault = false
+        config.showsCrop = .rectangle(ratio: 1.5)
+        config.library.mediaType = YPlibraryMediaType.photo
+        let imagePicker = YPImagePicker(configuration: config)
 
+        imagePicker.didFinishPicking { [unowned imagePicker] items, _ in
+            if let photo = items.singlePhoto {
+                // Image selected now start uploading process
+                print("Hi \(photo)")
+            }
+            imagePicker.dismiss(animated: true, completion: nil)
+        }
+
+        // This is where the app crashes
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
